0698c69f3e58f296ba74688254c684c15cc6a519
Joe Vanstory <joevanstory@gmail.com>
2023-09-15 23:14:11 -0400

    Update styles and Calculator

--- itis3135/scripts/calculator_try.js
+++ itis3135/scripts/calculator_try.js
@@ -1,21 +1,75 @@
-//creates a string to input the numbers
-let equation = "";
-      
-//adds the button's value that was pressed to the string in equation and displays it
-function display(number) {
-    equation += number;
-    document.getElementById('numbers').innerHTML = equation;
+let displayValue = '';
+let resultDisplayed = false;
+
+function appendToDisplay(value) {
+    if (resultDisplayed) {
+        clearDisplay();
+        resultDisplayed = false;
+    }
+    displayValue += value;
+    updateDisplay();
 }
-      
-//replaces the current string in equations with a blank one
-function clearNumbers() {
-    equation = "";
-    document.getElementById('numbers').innerHTML = equation;
+
+function clearDisplay() {
+    displayValue = '';
+    resultDisplayed = false;
+    updateDisplay();
 }
-      
-//takes the inputed buttons that are in the display/equation and evaluates the equation using eval()
+
 function calculate() {
-    let result = eval(equation);
-    document.getElementById('numbers').innerHTML = result;
-    equation = result;
+    try {
+        const sanitizedInput = sanitizeInput(displayValue);
+        const result = evaluateExpression(sanitizedInput);
+        displayValue = result.toString();
+        resultDisplayed = true;
+        updateDisplay();
+    } catch (error) {
+        displayValue = 'Error';
+        resultDisplayed = true;
+        updateDisplay();
+    }
 }
+
+function updateDisplay() {
+    document.getElementById('display').value = displayValue;
+}
+
+function sanitizeInput(input) {
+    const allowedCharactersRegex = /[0-9+\-*/.]/g;
+    return input.match(allowedCharactersRegex).join('');
+}
+
+function evaluateExpression(input) {
+    const operators = ['+', '-', '*', '/'];
+    const operatorRegex = new RegExp(`[${operators.join('')}]`);
+    const parts = input.split(operatorRegex);
+
+    if (parts.length !== 2) {
+        throw new Error('Invalid expression');
+    }
+
+    const operator = input.match(operatorRegex)[0];
+    const [num1, num2] = parts.map(parseFloat);
+
+    switch (operator) {
+        case '+':
+            return num1 + num2;
+        case '-':
+            return num1 - num2;
+        case '*':
+            return num1 * num2;
+        case '/':
+            if (num2 === 0) {
+                throw new Error('Division by zero');
+            }
+            return num1 / num2;
+        default:
+            throw new Error('Invalid operator');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('equals').addEventListener('click', function () {
+        calculate();
+    });
+});
